c426d2be762bfd7be5d03e3a86056bc4d34f6054
uditswaroopa <uditswaroopa@icloud.com>
2020-09-01 22:06:56 +0530

    HelloWorld App

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(), //Neha
-            body: Column(
-              children: <Widget>[
-                Text(
-                  'Hello',
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(8, 16, 35, 1),
+        appBar: AppBar(
+          title: Text('BBIRDS1'),
+          backgroundColor: Colors.grey[900],
+          centerTitle: true,
+        ), //Neha
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                'Hello',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'ArialBlack',
+                  color: Colors.greenAccent[400],
+                ),
+              ),
+              Image.network(url),
+              RaisedButton(
+                color: Colors.indigo[300],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                splashColor: Colors.indigoAccent[50], //Arnab
+                onPressed: () {},
+                child: Text(
+                  'Press Me',
                   style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'ArialBlack',
-                    color: Colors.greenAccent[400],
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
                   ),
                 ),
-                Image.network(url),
-                RaisedButton(
-                  color: Colors.indigoAccent[400],
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.red)),
-                      splashColor: Colors.indigoAccent[300], //Arnab
-                  onPressed: () {},
-                  child: Text('Click Me'),
-                )
-              ],
-            )));
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
