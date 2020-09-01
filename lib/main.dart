745d885be23e538ef152e70386d865976bd7e828
uditswaroopa <uditswaroopa@icloud.com>
2020-09-01 21:16:44 +0530

    I am saving in app1

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,36 @@
-import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+void main() => runApp(MyApp());
 
-class _MyAppState extends State<MyApp> {
-
-   ConfettiController _controllerBottomCenter;
-   
-   void initState() {
-    _controllerBottomCenter =
-        ConfettiController(duration: const Duration(seconds: 10));
-    super.initState();
-  }
-
-    void dispose() {
-    _controllerBottomCenter.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
+  final String url = 'https://media1.giphy.com/media/mf8UbIDew7e8g/200.gif';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: <Widget>[
-              Text('Hello World'),
-              SizedBox(height: 10.0),
-                      Align(
-          alignment: Alignment.center,
-          child: ConfettiWidget(
-            confettiController: _controllerBottomCenter,
-            blastDirectionality: BlastDirectionality
-                .explosive, // don't specify a direction, blast randomly
-            shouldLoop:
-                true, // start again as soon as the animation is finished
-            colors: const [
-              Colors.green,
-              Colors.blue,
-              Colors.pink,
-              Colors.orange,
-              Colors.purple
-            ], // manually specify the colors to be used
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: FlatButton(
-              onPressed: () {
-                _controllerBottomCenter.play();
-              },
-              child: Text('Press Me')
-            ),
-        ),
-          ],
-        )
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(), //Neha
+            body: Column(
+              children: <Widget>[
+                Text(
+                  'Hello',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'ArialBlack',
+                    color: Colors.greenAccent[400],
+                  ),
+                ),
+                Image.network(url),
+                RaisedButton(
+                  color: Colors.indigoAccent[400],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.red)),
+                      splashColor: Colors.indigoAccent[300], //Arnab
+                  onPressed: () {},
+                  child: Text('Click Me'),
+                )
+              ],
+            )));
   }
 }
