e32fbb77aa68c74c8ef1200de07c6dadbd559706
uditswaroopa <uditswaroopa@icloud.com>
2020-09-04 22:38:28 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:bluebirds/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return Container(
+      
     );
   }
 }
